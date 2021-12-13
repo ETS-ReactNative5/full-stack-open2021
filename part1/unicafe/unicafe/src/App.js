ad3068c00648884b79a0c18d7ca7def63f12736a
Federico Pregnolato <fed.pregnolato@gmail.com>
2021-12-13 15:09:08 +0100

    Completed step 1.7

--- part1/unicafe/unicafe/src/App.js
+++ part1/unicafe/unicafe/src/App.js
@@ -14,17 +14,37 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+
 
   return (
     <div>
       <h1>Give Feedback</h1>
-      <Button handleClick={() => setGood(good + 1)} text={'Good'} />
-      <Button handleClick={() => setNeutral(neutral + 1)} text={'Neutral'} />
-      <Button handleClick={() => setBad(bad + 1)} text={'Bad'} />
+      <Button handleClick={() => {
+        setGood(good + 1);
+        setAll(all + 1);
+        setAverage(average + 1)
+        }} 
+        text={'Good'} />
+      <Button handleClick={() => {
+        setNeutral(neutral + 1);
+        setAll(all + 1);
+        }} 
+        text={'Neutral'} />
+      <Button handleClick={() => {
+        setBad(bad + 1);
+        setAll(all + 1)
+        setAverage(average - 1)
+        }} 
+        text={'Bad'} />
       <h1>Statistics</h1>
       <p>Good: {good}</p>
       <p>Nutral: {neutral}</p>
       <p>Bad: {bad} </p>
+      <p>All: {all} </p>
+      <p>Average: {average / all} </p>
+      <p>Positive: {(good / all) * 100} %</p>
     </div>
   )
 }
