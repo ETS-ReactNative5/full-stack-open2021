c9ee0813bdd6e391d6eb555e88f063dd1eb6357c
fedpre <fed.pregnolato@gmail.com>
2021-12-15 19:22:56 +0100

    Completed step 2.10

--- part2/phonebook/phonebook-app/src/App.js
+++ part2/phonebook/phonebook-app/src/App.js
@@ -1,5 +1,51 @@
 import React, { useState } from 'react'
 
+const Filter = ({handleFilter}) => {
+  return (
+    <div>
+        Filter shown with <input onChange={handleFilter}/>
+    </div>
+  )
+}
+
+const PersonForm = ({ onSubmit, valueName, onChangeName, valueNumber, onChangeNumber }) => {
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+          <div>
+            name: <input value={valueName} onChange={onChangeName}/>
+          </div>
+          <div>
+            number: <input value={valueNumber} onChange={onChangeNumber}/>
+          </div>
+          <div>
+            <button type="submit">add</button>
+          </div>
+        </form>
+    </>
+  )
+}
+
+const Persons = ({ valueAll, filter, show }) => {
+  return (
+    <>
+      {show ? valueAll.map((person) => {
+        return (
+          <div key={person.name}>
+            {person.name} {person.number}
+          </div>
+        )
+      }) : filter().map((person) => {
+        return (
+          <div key={person.name}>
+            {person.name} {person.number}
+          </div>
+        )
+      })}
+    </>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456', id: 1 },
@@ -53,35 +99,24 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        Filter shown with <input onChange={handleFilterChange}/>
-      </div>
-      <h2> Add a new</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange}/>
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+
+      <Filter handleFilter={handleFilterChange} />
+
+      <h3> Add a new</h3>
+
+      <PersonForm 
+        onSubmit={addPerson} 
+        valueName={newName} 
+        onChangeName={handleNameChange}
+        valueNumber={newNumber}
+        onChangeNumber={handleNumberChange}
+      />
       <h2>Numbers</h2>
-      {showAll ? persons.map((person) => {
-        return (
-          <div key={person.name}>
-            {person.name} {person.number}
-          </div>
-        )
-      }) : filterPersonList().map((person) => {
-        return (
-          <div key={person.name}>
-            {person.name} {person.number}
-          </div>
-        )
-      })}
+      <Persons 
+        valueAll={persons}
+        filter={filterPersonList}
+        show={showAll}
+      />
     </div>
   )
 }
