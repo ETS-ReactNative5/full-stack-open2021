f35b1e3a1f3e2e4048b214fe1799044a81ae3e75
fedpre <fed.pregnolato@gmail.com>
2021-12-20 11:32:10 +0100

    completed 2.19

--- part2/phonebook/phonebook-app/src/App.js
+++ part2/phonebook/phonebook-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import './index.css'
 import personService from './services/persons'
 
 const Filter = ({handleFilter}) => {
@@ -71,7 +71,17 @@ const Button = ({ onDelete }) => {
   )
 }
 
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
 
+  return (
+    <div className='notification'>
+      {message}
+    </div>
+  )
+}
 
 const App = () => {
   const [persons, setPersons] = useState([]) 
@@ -79,6 +89,7 @@ const App = () => {
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [message, setMessage] = useState(null)
 
   useEffect(() => {
     personService
@@ -128,6 +139,7 @@ const App = () => {
             .getAll()
             .then(res => {
               setPersons(res.data)
+              
             })
             setNewName('')
             setNewNumber('')
@@ -135,6 +147,10 @@ const App = () => {
         }
       } else {
         setPersons(persons.concat(nameObject))
+        setMessage(`Added ${person.name}`)
+        setTimeout(() => {
+          setMessage(null)
+        }, 5000)
         setNewName('')
         setNewNumber('')
       }
@@ -150,7 +166,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-
+      <Notification message={message}/>
       <Filter handleFilter={handleFilterChange} />
 
       <h3> Add a new</h3>
