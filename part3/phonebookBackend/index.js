482e05ab9ae0d45dbc449a5ebd60205d52157357
fedpre <fed.pregnolato@gmail.com>
2021-12-27 20:40:49 +0100

    Finished step 3.9

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const res = require('express/lib/response')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
 // app.use(morgan('tiny'))
 
 morgan.token('content', (req, res) => {
