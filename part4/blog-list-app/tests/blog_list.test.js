1ac4a55402ef80a33fe64494c5aaf22dc2d56bd8
fedpre <fed.pregnolato@gmail.com>
2022-01-27 16:55:24 +0100

    Completed step 4.8

---
+++ part4/blog-list-app/tests/blog_list.test.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const helper = require('./blog_list_helper')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    const blogsObject = helper.initialBlogs.map(blog => new Blog(blog))
+    const promiseArray = blogsObject.map(blog => blog.save())
+    await Promise.all(promiseArray)
+})
+
+test('Blog list app returns the correct amount of blog posts', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(2)
+})
+
+afterAll(() => {
+    mongoose.connection.close()
+})
---
+++ part4/blog-list-app/tests/blog_list_helper.js
@@ -0,0 +1,18 @@
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+    {
+        title: 'Test 1',
+        author: 'Federico',
+        url: 'https://google.com',
+        likes: 5
+    },
+    {
+        title: 'Test 2',
+        author: 'Cristina',
+        url: 'https://amazon.com',
+        likes: 8
+    },
+]
+
+module.exports = { initialBlogs }
