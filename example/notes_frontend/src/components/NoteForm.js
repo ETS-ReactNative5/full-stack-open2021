0bbf1b78a9bd8929daea4f0334dfb648663db639
fedpre <fed.pregnolato@gmail.com>
2022-03-01 13:04:07 +0100

    Working on step 5 e2e testing

--- example/notes_frontend/src/components/NoteForm.js
+++ example/notes_frontend/src/components/NoteForm.js
@@ -23,6 +23,7 @@ const NoteForm = ({ createNote }) => {
       <h2> Create a new note</h2>
       <form onSubmit={addNote}>
         <input
+          id='new-note'
           value={newNote}
           onChange={handleChange}
         />
