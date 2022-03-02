90a9ad8e84c4fe016594536063facaf62b92e701
fedpre <fed.pregnolato@gmail.com>
2022-03-02 17:34:41 +0100

    Exercise 5.18 completed

--- part5/blog-frontend/cypress/integration/blog_app.spec.js
+++ part5/blog-frontend/cypress/integration/blog_app.spec.js
@@ -1,6 +1,12 @@
 describe('Blog app', function() {
     beforeEach(function() {
         cy.request('POST', 'http://localhost:3003/api/testing/reset')
+        const user = {
+            name: 'federico',
+            username: 'fedpre',
+            password: 'test1'
+        }
+        cy.request('POST', 'http://localhost:3003/api/users/', user)
         cy.visit('http://localhost:3000')
     })
 
@@ -9,5 +15,21 @@ describe('Blog app', function() {
         cy.contains('password')
         cy.get('#username').type('test 1')
         cy.get('#password').type('test 2')
+    })
+
+    describe('login', function() {
+        it('succeeeds with correct credentials', function() {
+            cy.get('#username').type('fedpre')
+            cy.get('#password').type('test1')
+            cy.contains('login').click()
+            cy.contains('federico logged in')
+        })
+
+        it('fails with incorrect credentials', function() {
+            cy.get('#username').type('fedpre2')
+            cy.get('#password').type('test2')
+            cy.contains('login').click()
+            cy.contains('Wrong Credentials')
+        })
     })
 })
