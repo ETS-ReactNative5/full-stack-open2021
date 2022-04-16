30346921e3cfc63c470e954e85d4350877978a21
fedpre <fed.pregnolato@gmail.com>
2022-04-16 15:14:05 +0200

    Added styling to error message

--- part5/blog-frontend/src/components/ErrorMessage.js
+++ part5/blog-frontend/src/components/ErrorMessage.js
@@ -7,9 +7,9 @@ const ErrorMessage = () => {
   if (message === null) {
     return null
   }
-
+  
   return (
-    <div className="error">
+    <div className='text-2xl text-pink-600 font-semibold border-solid rounded-md border-pink-500 px-6 py-3 w-full'>
       {message}
     </div>
   )
